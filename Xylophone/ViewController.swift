20652c998587cbf0ad374d59a8e25a4d87dbcd43
Vanam <raghavendra.gv.vanam@gmail.com>
2022-01-10 23:25:48 +0530

    Changes related to xylophone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func btnKeyPressed(_ sender: UIButton) {
+        let title = sender.currentTitle!
+        self.playSound(note: title)
 
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+        
+    }
     
+    func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+                
+    }
 
 }
 
